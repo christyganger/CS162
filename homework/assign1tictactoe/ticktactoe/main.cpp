f6338c6cea459bffe96091c10f2008c48557dce2
christyganger <42434778+christyganger@users.noreply.github.com>
2019-01-12 14:20:08 -0800

    newest version
    fixed, lack of win condition to skip the check for pc win and turn.. added comments.

--- homework/assign1tictactoe/ticktactoe/main.cpp
+++ homework/assign1tictactoe/ticktactoe/main.cpp
@@ -4,34 +4,55 @@ Written by : Christy Ganger
 Assignment 1
 Special thanks to http://www.cplusplus.com/reference/cstdlib/rand/ for reminding me on how to do the srand.
 */
+//includes
 #include <iostream>
 #include <stdio.h>      /* printf, scanf, puts, NULL */
 #include <stdlib.h>     /* srand, rand */
 #include <time.h>       /* time */
-#include <iomanip>
 
 using namespace std;
+
+//const
 const int firstsize = 10;
 const int secondsize =12;
+
+//prototypes
+
+//selection of user X or O
 void displayinstructions(char &xo);
+
+//show boards both play board and demonstration of number placement
 void showBoard(char boardcurrent[firstsize][secondsize],char ease_of_use_play_field_copy[firstsize][secondsize]);
+
+//gets player moves
 char getMove(char &xo);
+
+//checks for win condition
 bool checkWin(char &xo, char &xoPC, bool &PCwon, bool &won, const char *pointtofieldone, const char *pointtofieldtwo, const char *pointtofieldthree,
               const char *pointtofieldfour, const char *pointtofieldfive, const char *pointtofieldsix, const char *pointtofieldseven, const char *pointtofieldeight, const char *pointtofieldnine );
+
+// checks if field is taken, and if it's ok to draw the xo or ask for another move
 bool checkDraw(int movefield, char &xo, char *pointtofieldone, char *pointtofieldtwo, char *pointtofieldthree, char *pointtofieldfour,char *pointtofieldfive,
                 char *pointtofieldsix, char *pointtofieldseven, char *pointtofieldeight, char *pointtofieldnine );
+
+// checks for tie condition after move
 bool checkTieNoWin(const char *pointtofieldone, const char *pointtofieldtwo, const char *pointtofieldthree, const char *pointtofieldfour,
 const char *pointtofieldfive, const char *pointtofieldsix, const char *pointtofieldseven, const char *pointtofieldeight, const char *pointtofieldnine);
+
+// computer player and move
 bool PCplayer(char &xoPC, char *pointtofieldone, char *pointtofieldtwo, char *pointtofieldthree, char *pointtofieldfour,char *pointtofieldfive,
                 char *pointtofieldsix, char *pointtofieldseven, char *pointtofieldeight, char *pointtofieldnine);
+
 bool playagain();
 
 int main()
 {
 
+    //main game loop starts here
     do
 
     {
+        // variables reset on new game..
         char xo;
         char xoPC;
         bool done = false;
@@ -43,18 +64,25 @@ int main()
         bool check_tie = false;
         /* initialize random seed: */
         srand (time(NULL));
+
+        // randomises who goes first 1 = true player goes first 0 = comp goes first
         playersturn = rand() % 1 + 0;
+
+        //blank playboard
         char boardcurrent[firstsize][secondsize] = {{' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},{' ',' ',' ',' ','|',' ',' ',' ','|',' ',' ',' '},{' ',' ',' ',' ','|',' ',' ',' ','|',' ',' ',' '},
         {' ','_','_','_','|','_','_','_','|','_','_','_'},{' ',' ',' ',' ','|',' ',' ',' ','|',' ',' ',' '},{' ',' ',' ',' ','|',' ',' ',' ','|',' ',' ',' '},
         {' ','_','_','_','|','_','_','_','|','_','_','_'},{' ',' ',' ',' ','|',' ',' ',' ','|',' ',' ',' '},{' ',' ',' ',' ','|',' ',' ',' ','|',' ',' ',' '},
         {' ',' ',' ',' ','|',' ',' ',' ','|',' ',' ',' '} },*pointtofieldone, *pointtofieldtwo, *pointtofieldthree, *pointtofieldfour, *pointtofieldfive, *pointtofieldsix,
         *pointtofieldseven, *pointtofieldeight, *pointtofieldnine;
+
+        //blank demo board for placement
         char ease_of_use_play_field_copy[firstsize][secondsize] = {{' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},{' ',' ',' ',' ','|',' ',' ',' ','|',' ',' ',' '},{' ',' ',' ',' ','|',' ',' ',' ','|',' ',' ',' '},
         {' ','_','_','_','|','_','_','_','|','_','_','_'},{' ',' ',' ',' ','|',' ',' ',' ','|',' ',' ',' '},{' ',' ',' ',' ','|',' ',' ',' ','|',' ',' ',' '},
         {' ','_','_','_','|','_','_','_','|','_','_','_'},{' ',' ',' ',' ','|',' ',' ',' ','|',' ',' ',' '},{' ',' ',' ',' ','|',' ',' ',' ','|',' ',' ',' '},
         {' ',' ',' ',' ','|',' ',' ',' ','|',' ',' ',' '} },*point_to_field_ease_one, *point_to_field_ease_two, *point_to_field_ease_three, *point_to_field_ease_four,
          *point_to_field_ease_five, *point_to_field_ease_six, *point_to_field_ease_seven, *point_to_field_ease_eight, *point_to_field_ease_nine;
 
+        //requests player select x or o, then assigns computer to opposite.
         displayinstructions(xo);
          if(xo == 'X')
         {
@@ -65,6 +93,7 @@ int main()
             xoPC = 'X';
         }
 
+        // populates pointers to playboard position to avoid magic numbers
         pointtofieldone = &boardcurrent[2][2];
         pointtofieldtwo = &boardcurrent[2][6];
         pointtofieldthree = &boardcurrent[2][10];
@@ -75,6 +104,7 @@ int main()
         pointtofieldeight = &boardcurrent[8][6];
         pointtofieldnine = &boardcurrent[8][10];
 
+        //populates pointers to demo board position to avoid magic numbers
         point_to_field_ease_one = &ease_of_use_play_field_copy[2][2];
         point_to_field_ease_two = &ease_of_use_play_field_copy[2][6];
         point_to_field_ease_three = &ease_of_use_play_field_copy[2][10];
@@ -86,6 +116,7 @@ int main()
         point_to_field_ease_nine = &ease_of_use_play_field_copy[8][10];
 
 
+        // assigns demo board number's with pointers
         *point_to_field_ease_one = '1';
         *point_to_field_ease_two = '2';
         *point_to_field_ease_three = '3';
@@ -97,87 +128,147 @@ int main()
         *point_to_field_ease_nine = '9';
 
 
-
+        // game play loop for turns starts here.
         while(!done)
         {
+            //decides if it's players turn
             if (playersturn == true)
             {
-
+                //switch to computer turn after this turn.
                 playersturn = false;
+
+                // gets move and verifies it's valid.
                 verified_move  = checkDraw(getMove(xo), xo, pointtofieldone, pointtofieldtwo, pointtofieldthree, pointtofieldfour, pointtofieldfive,
                     pointtofieldsix, pointtofieldseven, pointtofieldeight, pointtofieldnine);
+
+                // if move is valid do this
                 if (verified_move  == true)
                 {
+                    //shows board and demo board
                     showBoard(boardcurrent,ease_of_use_play_field_copy);
 
+                    // assigns win condition
                     check_win = checkWin(xo, xoPC, PCwon, won, pointtofieldone, pointtofieldtwo, pointtofieldthree, pointtofieldfour,
                                  pointtofieldfive, pointtofieldsix, pointtofieldseven, pointtofieldeight, pointtofieldnine);
+
+                    //if there is win condition with this move..
                     if(check_win == true)
                     {
+                        //if computer wins this move..
                         if(PCwon == true)
                         {
                             cout << "You lose!" << "\n";
+                             //set turn condition loop off.
                              done = true;
+
+                             // ends turn condition loop
+                             continue;
                         }
+
+                        // if player wins
                         if(won == true)
                         {
                             cout << "You win!" << "\n";
+                             //set turn condition loop off.
                              done = true;
+
+                             // ends turn condition loop
+                             continue;
                         }
 
 
                     }
+                    // if win condition is off..
                     if(check_win == false)
                     {
+                        // check for tie condition assign true / false to check_tie
                         check_tie = checkTieNoWin(pointtofieldone, pointtofieldtwo, pointtofieldthree, pointtofieldfour,
                                  pointtofieldfive, pointtofieldsix, pointtofieldseven, pointtofieldeight, pointtofieldnine);
+
+                                 // if tie condition exists
                                  if(check_tie == true)
                                     {
                                         cout << "It's a Tie Game." << "\n";
+
+                                        // set turn condition loop off.
                                         done = true;
+
+                                        // ends turn condition loop.
+                                        continue;
                                     }
                     }
 
                 }
+                // if the move the player chose is invalid.
                 if (verified_move  == false)
                 {
+                    //show the playboard , and demo number position board.
                     showBoard(boardcurrent,ease_of_use_play_field_copy);
+
                     cout << "invalid move" << "\n";
+                    // gets a new move, verifies it with checkdraw to see if player move can be drawn and is valid.
                     checkDraw(getMove(xo), xo, pointtofieldone, pointtofieldtwo, pointtofieldthree, pointtofieldfour, pointtofieldfive,
                     pointtofieldsix, pointtofieldseven, pointtofieldeight, pointtofieldnine);
                 }
 
             }
+
+            // computer's turn!
             if (playersturn == false)
             {
+                // sets player's turn next loop
                 playersturn = true;
+
+                // decides computers next move, and that it's valid, then draws it
                 PCplayer(xoPC, pointtofieldone, pointtofieldtwo, pointtofieldthree, pointtofieldfour, pointtofieldfive, pointtofieldsix, pointtofieldseven,
                         pointtofieldeight, pointtofieldnine);
+
+                //shows board after computer move, plus playboard numbered reference board.
                 showBoard(boardcurrent,ease_of_use_play_field_copy);
+
+                // checks for win condition, assigns true for win, false for no win to check_win
                 check_win = checkWin(xo, xoPC, PCwon, won, pointtofieldone, pointtofieldtwo, pointtofieldthree, pointtofieldfour, pointtofieldfive, pointtofieldsix, pointtofieldseven,
                     pointtofieldeight, pointtofieldnine) ;
+
+                 // if a win condition on computer player is detected do this.
                 if(check_win == true)
                 {
+
+                    // if computer player won.
                     if(PCwon == true)
                     {
                         cout << "You lose!" << "\n";
+                        // end the turn condition loop.
                         done = true;
+                        continue;
                     }
+
+                    // if player won this round.. somehow?
                     if(won == true)
                     {
                         cout << "You win!" << "\n";
+                        // end the turn condition loop
                         done = true;
+                        continue;
                     }
 
                 }
+
+                //if win doeesn't result from computer turn..
                 if(check_win == false)
                     {
+                        // check for tie condition.
                         check_tie = checkTieNoWin(pointtofieldone, pointtofieldtwo, pointtofieldthree, pointtofieldfour,
                                  pointtofieldfive, pointtofieldsix, pointtofieldseven, pointtofieldeight, pointtofieldnine);
+
+                                 // if it's a tie, do this
                                  if(check_tie == true)
                                  {
                                      cout << "It's a Tie Game." << "\n";
+
+                                     //end turn condition loop
                                      done = true;
+                                     continue;
                                  }
                     }
 
@@ -187,29 +278,38 @@ int main()
 
 
         }
+        // check if player wants to play again for main loop, or end program.
     }while(!playagain());
     return 0;
 }
 
+// displays instructions, and asks users to select X or O, if it's lower case, make it capital, verify it's an X or O
 void displayinstructions(char &xo)
 {
     cout << "Welcome to Tic - Tac - Toe!" << "\n";
     cout << "To win you need to get a full row filled with your X or O, across, up and down, or diagnially" << "\n";
     cout << "To play First Select X or O" << "\n";
     cout << "Then Select which number of the field you'd like to place the X or O into." << "\n";
+
+    // loop until user gives valid x,o, X , O
     do
     {
         cout << "Would you like to play as X or O?" << "\n";
         cin >> xo;
-        if (xo != 'X' || xo != 'O')
+
+        // if lowercase,, .. convert to capital
+        if (xo == 'x' || xo == 'o')
         {xo = xo-32;}
 
+        //check for valid X O exit if it is.
     }while(!(xo == 'X' || xo == 'O'));
 
-
 }
+
+// Shows the playboard curent moves + available for next move without numbers, and one with numbers for reference.
 void showBoard(char boardcurrent[firstsize][secondsize],char ease_of_use_play_field_copy[firstsize][secondsize])
 {
+    // couts each field in order to create the playboard.
     for(int i =0; i < firstsize; i++)
     {
      for(int j =0; j < secondsize; j++)
@@ -220,7 +320,7 @@ void showBoard(char boardcurrent[firstsize][secondsize],char ease_of_use_play_fi
      cout << "\n";
     }
 
-
+    //couts the demo boar array, for each field to create the demo board
     for(int i =0; i < firstsize; i++)
     {
      for(int j =0; j < secondsize; j++)
@@ -231,13 +331,16 @@ void showBoard(char boardcurrent[firstsize][secondsize],char ease_of_use_play_fi
      cout << "\n";
     }
 }
+
+// gets players move. verifies it's actually 1-9 int if not, request it again clearing any cin error.
 char getMove(char &xo)
 {
+    // fields to clear or return on each move.
     int movefield;
     bool done_entering_field = false;
 
 
-
+    // loop of user input
     while(!done_entering_field)
     {
         cout << "Player " << xo <<" Please select the field you wish to use:" << "\n";
@@ -257,9 +360,12 @@ char getMove(char &xo)
 
     return movefield;
 }
+
+// checks for win condition across / up down/ diagnal for computer and player
 bool checkWin(char &xo, char &xoPC, bool &PCwon, bool &won, const char *pointtofieldone, const char *pointtofieldtwo, const char *pointtofieldthree, const char *pointtofieldfour,
 const char *pointtofieldfive, const char *pointtofieldsix, const char *pointtofieldseven, const char *pointtofieldeight, const char *pointtofieldnine )
 {
+    //return value if winner detected
     bool winner = false;
     //player across win
     if (*pointtofieldone == xo && *pointtofieldtwo == xo && *pointtofieldthree == xo)
@@ -351,9 +457,11 @@ const char *pointtofieldfive, const char *pointtofieldsix, const char *pointtofi
     return winner;
 }
 
+// checks that user's move is valid to draw to the board.
 bool checkDraw(int movefield, char &xo, char *pointtofieldone,  char *pointtofieldtwo, char *pointtofieldthree,  char *pointtofieldfour,
  char *pointtofieldfive, char *pointtofieldsix, char *pointtofieldseven, char *pointtofieldeight, char *pointtofieldnine )
 {
+    // sets valid move or not.
     bool isvalid = false;
 
 
@@ -441,49 +549,20 @@ bool checkDraw(int movefield, char &xo, char *pointtofieldone,  char *pointtofie
     return isvalid;
 }
 
-bool playagain()
-{
-    bool playagain = false;
-    bool cincheck = false;
-    char playing;
-    while(!cincheck)
-    {
-        cout << "Would you like to play again? Y/N" << "\n";
-        cin >> playing;
-        if(cin.fail())
-        {
-            cin.clear();
-            cin.ignore(2000,'\n');
-            continue;
-        }
-        switch(playing)
-        {
-        case 'Y':
-        case 'y':
-            playagain = false;
-            cincheck = true;
-            break;
-        case 'N':
-        case 'n':
-            playagain = true;
-            cincheck = true;
-            break;
-        default:
-            cout << "invalid input" << "\n";
-            break;
-        }
-    }
-    return playagain;
-}
-
+// computer player, random move decision checks that the move is valid, then submits it.
 bool PCplayer(char &xoPC,char *pointtofieldone, char *pointtofieldtwo, char *pointtofieldthree, char *pointtofieldfour,char *pointtofieldfive,
                 char *pointtofieldsix, char *pointtofieldseven, char *pointtofieldeight, char *pointtofieldnine)
 {
+    //check for valid move.
     bool isvalid = false;
 
+    //loop to decide move, and if it's valid if not, decides another move.
     do{
+
+            //randomly generates a new move
             int PCmoving = rand() % 9 + 1;
 
+        // move fields if field is blank, then it's valid.
         switch(PCmoving)
         {
         case 9:
@@ -577,14 +656,72 @@ bool PCplayer(char &xoPC,char *pointtofieldone, char *pointtofieldtwo, char *poi
 return isvalid;
 }
 
+//checks the false win condition, for a tie condition.
 bool checkTieNoWin(const char *pointtofieldone, const char *pointtofieldtwo, const char *pointtofieldthree, const char *pointtofieldfour,
 const char *pointtofieldfive, const char *pointtofieldsix, const char *pointtofieldseven, const char *pointtofieldeight, const char *pointtofieldnine)
 {
+    //returned tie condition bool
     bool IsItATieGame = false;
+    // looks for blank positions
     if (*pointtofieldone != ' ' && *pointtofieldtwo != ' ' && *pointtofieldthree != ' ' && *pointtofieldfour != ' ' && *pointtofieldfive != ' '
         && *pointtofieldsix != ' ' && *pointtofieldseven != ' ' && *pointtofieldeight != ' ' && *pointtofieldnine != ' ')
     {
+        // if no positions are left, then it's a tie.
         IsItATieGame = true;
     }
     return IsItATieGame;
+}
+
+// asks user if they want to play again for exit of main loop.
+bool playagain()
+{
+    //main play again bool to return.
+    bool playagain = false;
+
+    //loop bool for checking to play again or not with user input.
+    bool cincheck = false;
+
+    // user input variable characters
+    char playing;
+
+    //checks user input to play again
+    while(!cincheck)
+    {
+        cout << "Would you like to play again? Y/N" << "\n";
+        cin >> playing;
+        // if cin fails, do to improper input.
+        if(cin.fail())
+        {
+            // clear fail flag.
+            cin.clear();
+            // clear all input for 2000 characters
+            cin.ignore(2000,'\n');
+            // progress to next loop for new input
+            continue;
+        }
+
+        //switch and check for valid input
+        switch(playing)
+        {
+        case 'Y':
+        case 'y':
+            //set main loop to play again.
+            playagain = false;
+            // set input to valid, and exit playagain.
+            cincheck = true;
+            break;
+        case 'N':
+        case 'n':
+            // set main loop to not play again but exit..
+            playagain = true;
+            // set input to valid, exit playagain function.
+            cincheck = true;
+            break;
+        default:
+            // input error, start loop again.
+            cout << "invalid input" << "\n";
+            break;
+        }
+    }
+    return playagain;
 }
