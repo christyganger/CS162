773702b601a894ffc4a3dc846b7ba504b9091f6c
Christy Ganger <christyganger@gmail.com>
2019-01-30 15:47:58 -0800

    update

--- homework/lab4/lab4/main.cpp
+++ homework/lab4/lab4/main.cpp
@@ -37,7 +37,53 @@ int main()
     for(int j = 0; j < cin_buff_size; j++)
         cout << ptr_arr_new[j]<< "\n";
 
+//get in for 3
+int what_to_find[3];
+cout << "please enter digits to search array for: " << "\n";
+for(int n =0; n < 3; n++ )
+    cin >> what_to_find[n];
 
+    //binary search
+
+        for(int p =0; p < 3; p++)
+        {
+            int mid = 0;
+            int low = 0;
+            int high = 0;
+            high = cin_buff_size -1;
+            mid = cin_buff_size / 2;
+            bool done_with_find = false;
+            while(!done_with_find)
+            {
+            if(what_to_find[p] > ptr_arr_new[mid])
+            {
+                low = mid + 1;
+
+                mid = (low+ high) / 2;
+
+
+            }
+            if(what_to_find[p] < ptr_arr_new[mid])
+            {
+
+                high = mid-1;
+                mid = (low+ high)/2;
+
+            }
+            if(what_to_find[p] == ptr_arr_new[mid])
+            {
+                done_with_find=true;
+                cout << "found " << what_to_find[p] << "\n";
+
+            }
+            if(mid == cin_buff_size -1 || mid == 0)
+            {
+                cout << p << " not found" << "\n";
+                done_with_find = true;
+
+            }
+            }
+        }
 
     // clear heap array memory
     delete [] ptr_arr_new;
